f744b513f991e9ea9a5e42f4042d71ff13b046e0
Hoybaby <mbartek436@gmail.com>
2021-05-24 09:10:01 -0400

    added some new jsx to my component

---
+++ src/components/Playlist/Playlist.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Playlist.css';
+
+
+class Playlist extends React.Component {
+    render() {
+        return(
+            <div class="Playlist">
+                <input value="New Playlist"/>
+                <TrackList/> 
+                <button class="Playlist-save">SAVE TO SPOTIFY</button>
+            </div>
+        )
+    }
+}
