4fe11e49b6afcf57224acc39bc22052de39aaff6
Hoybaby <mbartek436@gmail.com>
2021-05-25 13:25:58 -0400

    making a utils folder so I can setup mySpotify api

---
+++ src/util/Spotify.js
@@ -0,0 +1,17 @@
+// Will be using implicit grant flow which will return a user's access token in the url
+
+
+let accessToken;
+
+const Spotify = {
+    getAccessToken() {
+        if(accessToken) {
+            return accessToken
+        }
+
+
+        // check for access token match
+    }
+}
+
+export default Spotify
