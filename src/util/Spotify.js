e2faefb6029afcfa5e3d21b6a2ac7f895bf47a1c
Hoybaby <mbartek436@gmail.com>
2021-05-27 00:28:50 -0400

    trying to build out my spotify function. making sure we want to clear paramaters from the URL after it expires

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -11,7 +11,19 @@ const Spotify = {
 
 
         // check for access token match
+        const accessToken = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = windw.location.href.match(/expires_in=([^&]*)/);
+
+        if(accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            
+            // we want to clear the parameters from the URL AFTER it expires. it helps prevent errors.
+            window.setTimeout(() => accessToken ='', expiresIn * 1000);
+            window.history.pushState('Access Toekn', null, '/');
+            return accessToken
+        }
     }
 }
 
-export default Spotify
+export default Spotify;
