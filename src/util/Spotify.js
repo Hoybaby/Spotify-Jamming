ab1926c90062857e59c913572352f20e32868646
Hoybaby <mbartek436@gmail.com>
2021-05-27 13:12:09 -0400

    doing a alot .thens to make sure the promises and the completeion of the spotify feature which is the whole application is working properly with the right functionality

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -44,17 +44,45 @@ const Spotify = {
             if(!jsonResponse.tracks) {
                 return [];
             }
-
-            return jsonResponse.tracks,items.map(track => ({
+            return jsonResponse.tracks.items.map(track => ({
                 id: track.id,
                 name: track.name,
                 artist:track.artists[0].name,
                 album: track.album.name,
                 uri: track.uri
+            }));
+        });
+    },
 
-            }))
-        })
+    savePlayList(name, trackUris) {
+        if(!name || !trackUris.length) {
+            return;
+        }
 
+        const accessToken = Spotify.getAccessToken();
+        const headers = {Authorization: `Bearer ${accessToken}`};
+        let userId;
+
+        return fetch('https://api.spotify.com/v1/me', {
+            headers: headers
+        }).then(response => response.json()).then(
+            jsonResponse => {
+                userId = jsonResponse.id
+                return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`), {
+                    headers: headers,
+                    method: 'POST',
+                    body: JSON.stringify({name: name})
+                }
+            }).then(response => response.json()).then(
+            jsonResponse => {
+                const playlistId = jsonResponse.id;
+                return fetch(`https://api.spotify.com/v1/users/${user_id}/playlists/${playlist_id}/tracks`, {
+                    headers: headers,
+                    method: 'POST',
+                    body: JSON.stringify({uris: trackUris})
+                })
+            }
+        ) 
     }
 }
 
